d2c4d46908cf7ac2c50cb2de6564a5a832901b55
Kevin Rockwood <kevin@rockwood.me>
2013-09-12 12:45:04 -0400

    Fix current workshop spec

--- vendor/engines/training_app/spec/models/training_app/workshop_spec.rb
+++ vendor/engines/training_app/spec/models/training_app/workshop_spec.rb
@@ -21,27 +21,27 @@ module TrainingApp
           @workshop.should_not be_full
         end
       end
-      
+
       describe "current" do
-        Given!(:old_workshop) { FactoryGirl.create(:workshop, end_date: Date.today - 5.days)}
-        Given!(:current_workshop) { FactoryGirl.create(:workshop, end_date: Date.today + 30.days)}
+        Given!(:old_workshop) { FactoryGirl.create(:workshop, start_date: Date.today - 5.days)}
+        Given!(:current_workshop) { FactoryGirl.create(:workshop, start_date: Date.today + 30.days)}
         Then { Workshop.current.should == current_workshop }
       end
-      
+
     end
-    
+
       describe "dates" do
-        
+
         context "in the same month" do
           Given(:workshop) { FactoryGirl.create(:workshop, start_date: Date.parse("2012-07-01"), end_date: Date.parse("2012-07-03"))}
           Then { workshop.dates.should == "July  1 -  3, 2012"}
         end
-        
+
         context "in the same month" do
           Given(:workshop) { FactoryGirl.create(:workshop, start_date: Date.parse("2012-07-29"), end_date: Date.parse("2012-08-01"))}
           Then { workshop.dates.should == "July 29 - August  1, 2012"}
         end
-        
+
       end
 
   end

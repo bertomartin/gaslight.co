1614a79f1d8319d07f892a9cc434d895feb5a122
Kevin Rockwood <kevin@rockwood.me>
2013-09-12 16:49:40 -0400

    Wip nesting registrations under courses

--- vendor/engines/training_app/app/controllers/training_app/registrations_controller.rb
+++ vendor/engines/training_app/app/controllers/training_app/registrations_controller.rb
@@ -1,17 +1,10 @@
 module TrainingApp
-  class RegistrationsController < InheritedResources::Base
+  class RegistrationsController < ApplicationController
 
     respond_to :json, :html
 
-    def show
-      @registration = Registration.find_by_code(params[:id])
-
-      if @registration.nil?
-        redirect_to registrations_path and return
-      end
-
-      render :index
-    end
+    expose(:course)
+    expose(:registration, finder: :find_by_code)
 
     def stats
       regs = Registration.where('created_at > ?', Date.new(2013, 5, 31))
---
+++ vendor/engines/training_app/spec/support/pages/new_registration_page.rb
@@ -0,0 +1,23 @@
+class NewRegistrationPage
+  include Capybara::DSL
+
+  def visit_page(course)
+    visit "/training/courses/#{course.id}/registrations/new"
+  end
+
+  def submit_form(user)
+    fill_in "name", with: "Bob Jones"
+    fill_in "email", with: "bobjones@example.com"
+    fill_in "Name on Card", with: "Bob Jones"
+    fill_in "Card Number", with: "4242424242424242"
+    fill_in "Expiration Month (MM)", with: "04"
+    fill_in "Expiration Year (YYYY)", with: "2015"
+    fill_in "CVC", with: "222"
+    click_button "Purchase"
+  end
+
+  def successful?
+    page.has_content("Im so proud of you")
+  end
+end
+

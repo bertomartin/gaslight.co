645715e54f6cc50f531f6537631a8e9418a1869a
Kevin Rockwood <kevin@rockwood.me>
2013-09-16 09:46:26 -0400

    Confirmations no longer belong to a workshop

--- vendor/engines/training_app/spec/mailers/training_app/confirmation_spec.rb
+++ vendor/engines/training_app/spec/mailers/training_app/confirmation_spec.rb
@@ -7,7 +7,7 @@ module TrainingApp
       TrainingApp::Registration.any_instance.stub(:charge_stripe).and_return(true)
     end
 
-    Given(:registration) { FactoryGirl.create(:registration, workshop: nil) }
+    Given(:registration) { FactoryGirl.create(:registration) }
     When(:registration_mail) { TrainingApp::Confirmation.registered(registration) }
     Then { registration_mail.body.should =~ /great day/i }
     Then { registration_mail.body.should =~ /#{registration.code}/ }

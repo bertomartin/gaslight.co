15fabf24d244d194521e752cc34406461016ce9d
Kevin Rockwood <kevin@rockwood.me>
2013-09-16 09:46:08 -0400

    Fix show course page

--- vendor/engines/training_app/app/models/training_app/course.rb
+++ vendor/engines/training_app/app/models/training_app/course.rb
@@ -3,6 +3,7 @@ module TrainingApp
 
     has_and_belongs_to_many :instructors
     has_many :registrations
+    has_many :workshops
 
     validate :start_date, :uniqueness => { :scope => :venue_id },
         :message => "One course at a time per venue, please."

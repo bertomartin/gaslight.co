99e7851c89bcaa2e32c8db89d766a8cc5d372f5f
Kevin Rockwood <kevin@rockwood.me>
2013-09-17 14:13:21 -0400

    Course index page course list

--- vendor/engines/training_app/spec/features/viewing_all_courses_spec.rb
+++ vendor/engines/training_app/spec/features/viewing_all_courses_spec.rb
@@ -1,28 +1,46 @@
 require 'spec_helper'
 
 feature "Viewing all courses" do
-  let(:venue) { FactoryGirl.create(:venue, city: "Chicago") }
   let(:course_index_page) { CourseIndexPage.new }
 
-  context "with an in person featured courses" do
-    let!(:in_person_featured_course) { FactoryGirl.create(:course, title: "In Person", synopsis: "synopsis", featured: true, start_date: Date.today, venue: venue) }
+  describe "featured courses" do
+    let(:venue) { FactoryGirl.create(:venue, city: "Chicago") }
 
-    scenario "viewing the featured course" do
-      course_index_page.visit_page
-      expect(course_index_page.featured_course_title).to eq("In Person")
-      expect(course_index_page.featured_course_synopsis).to eq("synopsis")
-      expect(course_index_page.featured_course_meta).to match("Chicago")
+    context "with an in person featured course" do
+      let!(:in_person_featured_course) { FactoryGirl.create(:course, title: "In Person", synopsis: "synopsis", featured: true, start_date: Date.today, venue: venue) }
+
+      scenario do
+        course_index_page.visit_page
+        expect(course_index_page.featured_course_title).to eq("In Person")
+        expect(course_index_page.featured_course_synopsis).to eq("synopsis")
+        expect(course_index_page.featured_course_meta).to match("Chicago")
+        expect(course_index_page.featured_course_button).to match("Reserve your seat")
+      end
+    end
+
+    context "with an online featured course" do
+      let!(:online_featured_course) { FactoryGirl.create(:course, title: "Online", synopsis: "synopsis", featured: true, start_date: nil, venue: nil) }
+
+      scenario do
+        course_index_page.visit_page
+        expect(course_index_page.featured_course_title).to eq("Online")
+        expect(course_index_page.featured_course_synopsis).to eq("synopsis")
+        expect(course_index_page.featured_course_meta).to eq("Online")
+        expect(course_index_page.featured_course_button).to match("Try now")
+      end
     end
   end
 
-  context "with an online featured course" do
-    let!(:online_featured_course) { FactoryGirl.create(:course, title: "Online", synopsis: "synopsis", featured: true, start_date: nil, venue: nil) }
+  describe "Course list" do
+    let!(:course1) { FactoryGirl.create(:course, title: "Course 1") }
+    let!(:course2) { FactoryGirl.create(:course, title: "Course 2") }
+    let!(:workshop1) { FactoryGirl.create(:workshop, course: course1) }
+    let!(:workshop2) { FactoryGirl.create(:workshop, course: course2) }
 
-    scenario "viewing the featured course" do
+    scenario do
       course_index_page.visit_page
-      expect(course_index_page.featured_course_title).to eq("Online")
-      expect(course_index_page.featured_course_synopsis).to eq("synopsis")
-      expect(course_index_page.featured_course_meta).to eq("Online")
+      expect(course_index_page.course_list_size).to eq(2)
+      expect(course_index_page.course_children_size).to eq(2)
     end
   end
 end
--- vendor/engines/training_app/spec/support/pages/course_index_page.rb
+++ vendor/engines/training_app/spec/support/pages/course_index_page.rb
@@ -20,5 +20,17 @@ class CourseIndexPage
   def featured_course_meta
     first(".featured-card__meta").text
   end
+
+  def featured_course_button
+    first(".featured-card__button").text
+  end
+
+  def course_list_size
+    all(".course-list__course").size
+  end
+
+  def course_children_size
+    all(".course__child").size
+  end
 end
 

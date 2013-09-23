f30a817244ee9e052ccc1175a0470064a2737ea6
Kevin Rockwood <kevin@rockwood.me>
2013-09-23 13:34:49 -0400

    Pretty up the courses admin

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register TrainingApp::Course do
   menu label: 'Courses', parent: 'Training'
+
+  index do
+    column :title
+    column :parent_course
+    column :start_date
+    column :end_date
+    column :price
+    column :featured
+    column :online
+
+    default_actions
+  end
 end

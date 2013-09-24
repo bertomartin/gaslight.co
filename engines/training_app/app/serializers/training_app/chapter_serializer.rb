de24b723f11a9ae0e232eb1c4222cf2ddd35040d
Kevin Rockwood <kevin@rockwood.me>
2013-09-24 16:35:34 -0400

    Remove urls from chapter when serializing without a registration

--- engines/training_app/app/serializers/training_app/chapter_serializer.rb
+++ engines/training_app/app/serializers/training_app/chapter_serializer.rb
@@ -1,5 +1,13 @@
 module TrainingApp
   class ChapterSerializer < ActiveModel::Serializer
     attributes :id, :title, :title, :order, :description, :video_url, :code_url, :poster_url, :section_id
+
+    def attributes
+      attrs = super
+      if registration.nil? && !attrs["demo"]
+        [:code_url, :video_url, :poster_url].each { |attr| attrs[attr] = nil}
+      end
+      attrs
+    end
   end
 end

112f76e304a5a18ea943f62480dbfc0a5529e82e
Kevin Rockwood <kevin@rockwood.me>
2013-09-24 13:24:49 -0400

    Add default mailer host to staging and production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -73,6 +73,8 @@ Gaslight::Application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: 'gaslight-staging.herokuapp.com' }
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5

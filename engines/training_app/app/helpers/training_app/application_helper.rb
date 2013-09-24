cc5b65fef4ed9f8cfe741c7a42fb9b9b0a721f1d
Kevin Rockwood <kevin@rockwood.me>
2013-09-24 09:56:43 -0400

    Add markdown method to application helper

--- engines/training_app/app/helpers/training_app/application_helper.rb
+++ engines/training_app/app/helpers/training_app/application_helper.rb
@@ -1,2 +1,8 @@
+require "redcarpet"
+
 module TrainingApp::ApplicationHelper
+  def markdown(text)
+    @renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+    @renderer.render(text).html_safe
+  end
 end

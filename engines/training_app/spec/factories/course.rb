7d42ffbfd606bb4c934151ad69d1d52fec1921bb
Kevin Rockwood <kevin@rockwood.me>
2013-09-24 10:36:04 -0400

    Remove course.description_close

--- engines/training_app/spec/factories/course.rb
+++ engines/training_app/spec/factories/course.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :course, class: TrainingApp::Course do
     title "My Awesome Class"
     description_main "A great technology to learn with!"
-    description_close "Why not try it?"
     start_date { 1.day.ago }
     end_date { Date.today + 1.month }
     price 199

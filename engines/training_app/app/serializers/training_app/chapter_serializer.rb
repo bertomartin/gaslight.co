2498bfb83030d7d34645b8676667965e2ca00f14
Kevin Rockwood <kevin@rockwood.me>
2013-09-25 14:27:42 -0400

    Add a disabled attribute to chapter json
    In ember we need to determine how to activate a chapter. demo == true or disabled = false

--- engines/training_app/app/serializers/training_app/chapter_serializer.rb
+++ engines/training_app/app/serializers/training_app/chapter_serializer.rb
@@ -1,10 +1,11 @@
 module TrainingApp
   class ChapterSerializer < ActiveModel::Serializer
-    attributes :id, :title, :order, :description, :video_url, :code_url, :poster_url, :section_id
+    attributes :id, :title, :order, :description, :video_url, :code_url, :poster_url, :section_id, :demo
 
     def attributes
       attrs = super
       if registration.nil? && !object.demo?
+        attrs[:disabled] = true
         [:code_url, :video_url, :poster_url].each { |attr| attrs[attr] = nil}
       end
       attrs

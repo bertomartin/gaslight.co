1f6c1ebf96519c43c7acfc238e8db6fe12a53117
Kevin Rockwood <kevin@rockwood.me>
2013-09-25 09:29:55 -0400

    Remove unused routes

--- engines/training_app/config/routes.rb
+++ engines/training_app/config/routes.rb
@@ -1,8 +1,4 @@
 TrainingApp::Engine.routes.draw do
-  #ActiveAdmin.routes(self)
-  #devise_for :admin_users, ActiveAdmin::Devise.config
-
-  resources :workshops
   resources :courses, only: [:index, :show] do
     resources :registrations do
       get :stats, on: :collection
@@ -10,6 +6,7 @@ TrainingApp::Engine.routes.draw do
   end
 
   namespace :api, defaults: {format: :json} do
+    resources :courses
     resources :sections
     resources :chapters
   end

29b48dc906f5a65631d61ae3d3d97985c0595ab0
Kevin Rockwood <kevin@rockwood.me>
2013-09-25 15:16:59 -0400

    Restructure demo spec

--- engines/training_app/spec/features/demoing_a_course_spec.rb
+++ engines/training_app/spec/features/demoing_a_course_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 
 feature "Demoing a course", js: true do
+  let(:classroom_page) { ClassroomPage.new }
+
   let!(:course) { FactoryGirl.create(:course, online: true) }
   let!(:section) { FactoryGirl.create(:section, course: course) }
-  let!(:demo_chapter) { FactoryGirl.create(:chapter, title: "Demo Chapter", demo: true, section: section) }
-  let!(:restricted_chapter) { FactoryGirl.create(:chapter, title: "Restricted Chapter", section: section) }
-  let(:course_demo_page) { CourseDemoPage.new }
-  scenario "with restricted and demo chapters" do
-    course_demo_page.visit_page(course)
-    expect(course_demo_page.disabled_chapters).to include(restricted_chapter.title)
-    expect(course_demo_page.disabled_chapters).to_not include(demo_chapter.title)
+  let!(:demo_chapter) { FactoryGirl.create(:chapter, title: "Demo", section: section, demo: true, code_url: "http://test.com") }
+  let!(:restricted_chapter) { FactoryGirl.create(:chapter, title: "Restricted", section: section) }
+
+  describe "restricted nav links" do
+    scenario "should be disabled" do
+      classroom_page.visit_page(course)
+      expect(classroom_page.disabled_chapters).to include(restricted_chapter.title)
+      expect(classroom_page.disabled_chapters).to_not include(demo_chapter.title)
+    end
   end
 end
--- engines/training_app/spec/support/pages/classroom_page.rb
+++ engines/training_app/spec/support/pages/classroom_page.rb
@@ -1,13 +1,21 @@
-class CourseDemoPage
+class ClassroomPage
   include Capybara::DSL
 
   def visit_page(course)
     visit "/training/classroom/#/courses/#{course.id}"
   end
 
+  def visit_chapter(chapter)
+    visit "/training/classroom/#/courses/#{chapter.section.course.id}/chapters/#{chapter.id}"
+  end
+
   def disabled_chapters
     find(".subnav__item--disabled") # this will wait until ember renders
     all(".subnav__item--disabled").collect(&:text)
+  end
+
+  def code_url
+    find('iframe')[:src]
   end
 end
 

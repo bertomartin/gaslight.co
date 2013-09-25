830dff7d2161c91f56ea33b6053b4fa56994d260
Kevin Rockwood <kevin@rockwood.me>
2013-09-25 15:25:27 -0400

    Test that the demo chapter is shown

--- engines/training_app/spec/features/demoing_a_course_spec.rb
+++ engines/training_app/spec/features/demoing_a_course_spec.rb
@@ -5,14 +5,24 @@ feature "Demoing a course", js: true do
 
   let!(:course) { FactoryGirl.create(:course, online: true) }
   let!(:section) { FactoryGirl.create(:section, course: course) }
-  let!(:demo_chapter) { FactoryGirl.create(:chapter, title: "Demo", section: section, demo: true, code_url: "http://test.com") }
-  let!(:restricted_chapter) { FactoryGirl.create(:chapter, title: "Restricted", section: section) }
 
-  describe "restricted nav links" do
+  describe "viewing restricted nav links" do
+    let!(:demo_chapter) { FactoryGirl.create(:chapter, title: "Demo", demo: true, section: section) }
+    let!(:restricted_chapter) { FactoryGirl.create(:chapter, title: "Restricted", section: section) }
+
     scenario "should be disabled" do
       classroom_page.visit_page(course)
       expect(classroom_page.disabled_chapters).to include(restricted_chapter.title)
       expect(classroom_page.disabled_chapters).to_not include(demo_chapter.title)
+    end
+  end
+
+
+  describe "viewing a chapter" do
+    let!(:demo_chapter) { FactoryGirl.create(:chapter, title: "Demo", demo: true, code_url: "http://test.com/") }
+    scenario "should exist" do
+      classroom_page.visit_chapter(demo_chapter)
+      expect(classroom_page.code_url).to eq("http://test.com/")
     end
   end
 end

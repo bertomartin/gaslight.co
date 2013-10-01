f1f8580ba1729d63d259a515d9da3106281466ec
Kevin Rockwood <kevin@rockwood.me>
2013-10-01 09:50:12 -0400

    Refactor experiencing a course test prose

--- engines/training_app/spec/features/experiencing_an_online_course_spec.rb
+++ engines/training_app/spec/features/experiencing_an_online_course_spec.rb
@@ -1,39 +1,47 @@
 require 'spec_helper'
 
 feature "Experiencing an online course", js: true do
-  let(:chapter) { FactoryGirl.create(:chapter, title: "Test Chapter") }
-  let(:course) { chapter.section.course }
+  let(:course) { FactoryGirl.create(:course, title: "Test Course") }
   let(:registration) { FactoryGirl.create(:registration, course: course, code: "123") }
   let(:classroom_page) { ClassroomPage.new }
 
-  scenario "with a valid code" do
-    classroom_page.visit_course(registration.course, registration.code)
-    expect(classroom_page.title).to eq("#{chapter.section.title} - #{chapter.title}")
-  end
+  describe "viewing a course" do
+    context "with a valid code" do
+      scenario "isn't in demo mode" do
+        classroom_page.visit_course(course, registration.code)
+        expect(classroom_page.title).to eq("#{course.title}")
+      end
+    end
 
-  scenario "with an invalid code" do
-    registration.code = "invalid_code"
-    classroom_page.visit_course(registration.course, registration.code)
-    expect(classroom_page).to be_showing_demo
-  end
-
-  describe "seeing only this courses chapters" do
-    let!(:unrelated_chapter) { FactoryGirl.create(:chapter, title: "Another Chapter") }
-
-    scenario "should not see the unrelated chapter" do
-      classroom_page.visit_course(registration.course, registration.code)
-      expect(classroom_page.chapters.count).to eq(1)
+    context "with an invalid code" do
+      scenario "is in demo mode" do
+        classroom_page.visit_course(course, "invalid_code")
+        expect(classroom_page).to be_showing_demo
+      end
     end
   end
 
-  describe "chapter ordering" do
-    let(:section) { FactoryGirl.create(:section) }
-    let!(:chapter3) { FactoryGirl.create(:chapter, title: "Three", sort_order: nil, section: section, demo: true) }
-    let!(:chapter2) { FactoryGirl.create(:chapter, title: "Two", sort_order: 2, section: section) }
-    let!(:chapter1) { FactoryGirl.create(:chapter, title: "One", sort_order: 1, section: section) }
-    scenario "should be ordered by the order property" do
-      classroom_page.visit_course(section.course)
-      expect(classroom_page.chapters).to eq(["One", "Two", "Three"])
+  describe "sidebar navigation" do
+    let(:section) { FactoryGirl.create(:section, course: course) }
+
+    describe "unrelated chapters" do
+      let!(:chapter) { FactoryGirl.create(:chapter, title: "Test Chapter", section: section) }
+      let!(:unrelated_chapter) { FactoryGirl.create(:chapter, title: "Another Chapter") }
+
+      scenario "should not be visible" do
+        classroom_page.visit_course(course, registration.code)
+        expect(classroom_page.chapters).to eq(["Test Chapter"])
+      end
+    end
+
+    describe "chapter ordering" do
+      let!(:chapter3) { FactoryGirl.create(:chapter, title: "Three", sort_order: nil, section: section) }
+      let!(:chapter2) { FactoryGirl.create(:chapter, title: "Two", sort_order: 2, section: section) }
+      let!(:chapter1) { FactoryGirl.create(:chapter, title: "One", sort_order: 1, section: section) }
+      scenario "should be ordered by the order property" do
+        classroom_page.visit_course(course, registration.code)
+        expect(classroom_page.chapters).to eq(["One", "Two", "Three"])
+      end
     end
   end
 end

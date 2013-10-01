9855cd669335189add6cef6409c9f10c4a3e5dbc
Kevin Rockwood <kevin@rockwood.me>
2013-10-01 10:19:07 -0400

    Add course to chapter

--- engines/training_app/app/models/training_app/chapter.rb
+++ engines/training_app/app/models/training_app/chapter.rb
@@ -1,6 +1,7 @@
 module TrainingApp
   class Chapter < ActiveRecord::Base
     belongs_to :section
+    has_one :course, through: :section
     validates :section, presence: true
     default_scope { order('sort_order') }
   end

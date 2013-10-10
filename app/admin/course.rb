f29fe484da555a1bad403419ebc777d5cf874b89
Chris Moore <cdmwebs@gmail.com>
2013-10-10 10:54:16 -0400

    Clean up Course show page in ActiveAdmin

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -26,6 +26,33 @@ ActiveAdmin.register TrainingApp::Course do
     default_actions
   end
 
+  show do |course|
+    attributes_table do
+      row :id
+      row :title
+      row(:description_main) { pre course.description_main }
+      row(:description_html) { raw Renderer.render(course.description_main) }
+      row :synopsis
+      row :start_date
+      row :end_date
+      row :price
+      row :venue
+      row(:image) { image_tag(course.image_url) }
+      row :featured
+      row :capacity
+      row :early_bird_price
+      row :early_bird_end_date
+      row :registration_link
+      row :parent_course
+      row :online
+      row :color_primary
+      row :color_secondary
+      row :active
+      row :created_at
+      row :updated_at
+    end
+  end
+
   form do |f|
     f.inputs "Course Details" do
       f.input :venue

efcd4edbc39e6cb4f0d3827ab024061b20e990c5
Chris Moore <cdmwebs@gmail.com>
2013-10-12 12:23:50 -0400

    Allow venue attributes to be updated

--- app/admin/venue.rb
+++ app/admin/venue.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register TrainingApp::Venue do
   menu label: 'Venues', parent: 'Training'
+
+  controller do
+    def permitted_params
+      params.permit(training_app_venue: [:name, :address, :blurb, :city])
+    end
+  end
 end
+

d369067c07d2c0e8c1faa7a31db2a0d15a934d39
Kristin Lasita <kclasita@gmail.com>
2013-10-21 16:23:43 -0400

    Convert training app courses to new markup

--- engines/training_app/app/controllers/training_app/courses_controller.rb
+++ engines/training_app/app/controllers/training_app/courses_controller.rb
@@ -4,7 +4,6 @@ module TrainingApp
     respond_to :html
 
     expose(:courses) { Course.active.top_level }
-    expose(:featured_courses) { Course.active.featured }
     expose(:course)
   end
 end

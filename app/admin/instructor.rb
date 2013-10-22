b76bf9a6c720029eb472b6d9a90500c7c24bc248
Kevin Rockwood <kevin@rockwood.me>
2013-10-22 15:47:49 -0400

    Add strong params to admin pages

--- app/admin/instructor.rb
+++ app/admin/instructor.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register TrainingApp::Instructor do
   menu label: 'Instructors', parent: 'Training'
+
+  controller do
+    def permitted_params
+      params.permit(training_app_instructor: [:name, :image_url, :bio])
+    end
+  end
 end
--- app/admin/section.rb
+++ app/admin/section.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register TrainingApp::Section do
   menu label: 'Sections', parent: 'Training'
+
+  controller do
+    def permitted_params
+      params.permit!
+    end
+  end
 end

dd2ac509576b05c50eb79fd52a974b7217a217b0
Kevin Rockwood <kevin@rockwood.me>
2013-10-22 15:32:25 -0400

    Update course show spec

--- engines/training_app/spec/features/viewing_a_course_spec.rb
+++ engines/training_app/spec/features/viewing_a_course_spec.rb
@@ -15,25 +15,10 @@ feature "Viewing a course" do
       end
     end
 
-    describe "viewing registration_links" do
-      let!(:child_course) { FactoryGirl.create(:course, parent_course: parent_course) }
-      let!(:sibling_course1) { FactoryGirl.create(:course, parent_course: parent_course, registration_link: "foo.com") }
-      let!(:sibling_course2) { FactoryGirl.create(:course, parent_course: parent_course, registration_link: "bar.com") }
-
-      context "on a parent course" do
-        scenario "shows the children's registration links" do
-          course_show_page.visit_page(parent_course)
-          expect(course_show_page).to have_registration_link("foo.com")
-          expect(course_show_page).to have_registration_link("bar.com")
-        end
-      end
-
-      context "on a child course" do
-        scenario "shows the sibling's registration links" do
-          course_show_page.visit_page(child_course)
-          expect(course_show_page).to have_registration_link("foo.com")
-          expect(course_show_page).to have_registration_link("bar.com")
-        end
+    describe "viewing the registration link" do
+      scenario do
+        course_show_page.visit_page(parent_course)
+        expect(course_show_page).to have_registration_link("google.com")
       end
     end
   end

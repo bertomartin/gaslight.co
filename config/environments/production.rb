fa19823efe97b7ea86a08fd9fc0a57cdf22d2f81
Kevin Rockwood <kevin@rockwood.me>
2013-10-24 10:21:25 -0400

    Add modernizr.js to precompile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Gaslight::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( training_app/classroom.css training_app/classroom.js)
+  config.assets.precompile += %w( modernizr.js training_app/classroom.css training_app/classroom.js)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

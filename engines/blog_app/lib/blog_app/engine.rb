f306b831cc3bd0df7efad8c5afbe19de3cc89cc1
Kevin Rockwood <kevin@rockwood.me>
2013-10-29 13:20:09 -0400

    Setup rspec

--- engines/blog_app/lib/blog_app/engine.rb
+++ engines/blog_app/lib/blog_app/engine.rb
@@ -1,5 +1,18 @@
+require "haml"
+require "haml-rails"
+require "decent_exposure"
+require "sass-rails"
+require "compass-rails"
+
 module BlogApp
   class Engine < ::Rails::Engine
     isolate_namespace BlogApp
+
+    config.generators do |g|
+      g.test_framework      :rspec,        fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
---
+++ engines/blog_app/spec/models/blog_app/post_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+module BlogApp
+  describe "test" do
+    it "should be true" do
+      expect(true).to eq(true)
+    end
+  end
+end

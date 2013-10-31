bf681f6a05cca6d35f88d6b67138a20e2414e635
Kevin Rockwood <kevin@rockwood.me>
2013-10-31 11:24:24 -0400

    Clean up main_app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Gaslight::Application.routes.draw do
   mount TrainingApp::Engine, at: 'training'
+
   mount BlogApp::Engine, at: 'blog'
   get 'authors', to: redirect('/blog/authors')
+  get 'post/:id(/:slug)', to: 'blog_app/posts#old' # handle old tumblr urls
 
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -12,9 +14,7 @@ Gaslight::Application.routes.draw do
   get 'sitemap.xml' => 'sitemaps#index', as: 'sitemap', defaults: { format: "xml" }
 
   get 'about', to: 'pages#about'
-  root to: 'high_voltage/pages#show', id: 'home'
-
-  get 'post/:id(/:slug)', to: 'blog_app/posts#old' # handle old tumblr urls
+  root to: 'pages#show', id: 'home'
 
   get "/404", to: "errors#not_found"
   get "/500", to: "errors#server_error"
